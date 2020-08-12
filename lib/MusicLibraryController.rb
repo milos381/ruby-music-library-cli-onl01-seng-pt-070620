92f7c08d23621fbfa8fc86d0f530abb5dbc82386
Milos Popovic <milos_popovic@outlook.com>
2020-08-12 04:47:24 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -12,7 +12,7 @@ class MusicLibraryController
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
     puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list by artist'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
     puts "To list all of the songs of a particular genre, enter 'list by genre'."
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
