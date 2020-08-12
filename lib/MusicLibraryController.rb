de8cc4fbb8a0795445c19141685a81f99774fbec
Milos Popovic <milos_popovic@outlook.com>
2020-08-12 04:49:18 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -1,37 +1,41 @@
+require 'pry'
 class MusicLibraryController
-  attr_accessor :path
+
   def initialize(path = "./db/mp3s")
-    @path = path
+
     MusicImporter.new(path).import
   end
   def call
-    input = gets.strip
-    if input != exit
+    input = ""
+    while input != "exit"
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
     puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list by artist'."
-    puts "To list all of the songs of a particular genre, enter 'list by genre'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+    input = gets.strip
 
-    if input == "list songs"
-      list_songs
-    elsif input == "list artists"
-      list_artists
-    elsif input == "list genres"
-      list_genres
-    elsif input == "list by artist"
-      list_by_artist
-    elsif input == "list by genre"
-      list_by_genre
-    elsif input == "play song"
-      play_song
-    end
-  end
-  end
+         case input
+             when "list songs"
+               list_songs
+             when "list artists"
+               list_artists
+             when "list genres"
+               list_genres
+             when "list artist"
+               list_songs_by_artist
+             when "list genre"
+               list_songs_by_genre
+             when "play song"
+               play_song
+             end
+         end
+       end
+
   # def files #returns all of the imported filenames.
   #    Dir.entries(@path).select! {|entry| entry.end_with?(".mp3")}
   # end
@@ -41,25 +45,25 @@ class MusicLibraryController
   #     Song.create_from_filename(filename)
   #   end
   # end
-  def list_artists
-    Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |index, name|
-      puts "#{index + 1}. #{name.name}"
-    end
-
-  end
   def list_songs
-    Songs.all.sort{|a, b| a.name <=> b.name}.each_with_index do |index, name|
-      puts "#{index + 1}. #{name.name}"
+    Song.all.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
+      puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
     end
-
   end
+
+  def list_artists
+    Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |a, i|
+      puts "#{i+1}. #{a.name}"
+    end
+  end
+
   def list_genres
     Genre.all.sort{|a, b| a.name <=> b.name}.each_with_index do |g, i|
       puts "#{i+1}. #{g.name}"
     end
   end
 
-  def list_by_artist
+  def list_songs_by_artist
     puts "Please enter the name of an artist:"
     input = gets.strip
 
@@ -70,7 +74,7 @@ class MusicLibraryController
     end
   end
 
-  def list_by_genre
+  def list_songs_by_genre
     puts "Please enter the name of a genre:"
     input = gets.strip
 
