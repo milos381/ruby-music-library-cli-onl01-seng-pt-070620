7ab3a19eec9b52f8d64125abb436b19ac984b7a9
Milos Popovic <milos_popovic@outlook.com>
2020-08-12 04:43:22 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -1,12 +1,13 @@
+require 'pry'
 class MusicLibraryController
-  attr_accessor :path
+
   def initialize(path = "./db/mp3s")
-    @path = path
+
     MusicImporter.new(path).import
   end
   def call
-    input = gets.strip
-    if input != exit
+    input = ""
+    while input != "exit"
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -16,7 +17,7 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-
+    input = gets.strip
     if input == "list songs"
       list_songs
     elsif input == "list artists"
@@ -24,6 +25,7 @@ class MusicLibraryController
     elsif input == "list genres"
       list_genres
     elsif input == "list by artist"
+      binding.pry
       list_by_artist
     elsif input == "list by genre"
       list_by_genre
