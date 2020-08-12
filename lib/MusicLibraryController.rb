6dc43273af81e7c1a82bc0c73f5f6f1672f870db
Milos Popovic <milos_popovic@outlook.com>
2020-08-12 04:45:37 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -6,35 +6,36 @@ class MusicLibraryController
     MusicImporter.new(path).import
   end
   def call
-    input = ""
-    while input != "exit"
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list by artist'."
-    puts "To list all of the songs of a particular genre, enter 'list by genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-    input = gets.strip
-    case input
-        when "list songs"
-          list_songs
-        when "list artists"
-          list_artists
-        when "list genres"
-          list_genres
-        when "list artist"
-          list_songs_by_artist
-        when "list genre"
-          list_songs_by_genre
-        when "play song"
-          play_song
-        end
-    end
-  end
-  end
+     input = gets.strip
+
+     if input != exit
+
+     puts "Welcome to your music library!"
+     puts "To list all of your songs, enter 'list songs'."
+     puts "To list all of the artists in your library, enter 'list artists'."
+     puts "To list all of the genres in your library, enter 'list genres'."
+     puts "To list all of the songs by a particular artist, enter 'list artist'."
+     puts "To list all of the songs of a particular genre, enter 'list genre'."
+     puts "To play a song, enter 'play song'."
+     puts "To quit, type 'exit'."
+     puts "What would you like to do?"
+
+     case input
+         when "list songs"
+           list_songs
+         when "list artists"
+           list_artists
+         when "list genres"
+           list_genres
+         when "list artist"
+           list_songs_by_artist
+         when "list genre"
+           list_songs_by_genre
+         when "play song"
+           play_song
+         end
+     end
+   end
   # def files #returns all of the imported filenames.
   #    Dir.entries(@path).select! {|entry| entry.end_with?(".mp3")}
   # end
