57326bed1249d587090b8561f9860bb2bbe7c0ff
Milos Popovic <milos_popovic@outlook.com>
2020-08-12 04:48:30 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -45,18 +45,18 @@ class MusicLibraryController
   #     Song.create_from_filename(filename)
   #   end
   # end
-  def list_artists
-    Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |index, name|
-      puts "#{index + 1}. #{name.name}"
-    end
-
-  end
   def list_songs
-    Songs.all.sort{|a, b| a.name <=> b.name}.each_with_index do |index, name|
-      puts "#{index + 1}. #{name.name}"
+    Song.all.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
+      puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
     end
-
   end
+
+  def list_artists
+    Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |a, i|
+      puts "#{i+1}. #{a.name}"
+    end
+  end
+
   def list_genres
     Genre.all.sort{|a, b| a.name <=> b.name}.each_with_index do |g, i|
       puts "#{i+1}. #{g.name}"
